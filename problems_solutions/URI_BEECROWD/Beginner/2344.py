f4fa28e9931d3cf750f779a5357a5b4b3696f6f5
Matheus Miranda <matheusmirandalacerda@gmail.com>
2022-01-14 12:46:39 -0300

    Add solution for 2344 in python

---
+++ problems_solutions/URI_BEECROWD/Beginner/2344.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+if n <= 100 and n >= 86:
+    print('A')
+elif n <= 85 and n >= 62:
+    print('B')
+elif n <= 60 and n >= 35:
+    print('C')
+elif n <= 35 and n >= 1:
+    print('D')
+else:
+    print('E')
